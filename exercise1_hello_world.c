69160529fa111d47d8504b8237c449daa79f8408
Kimberly <kjacques709@gmail.com>
2021-05-19 17:57:02 +0200

    Ajout des commentaires

--- exercise1_hello_world.c
+++ exercise1_hello_world.c
@@ -1,3 +1,16 @@
+/*
+exercise1_hello_world.c
+--------------
+by eldr@e@
+--------------
+Main Program: Affiche "Hello World!" dans la Console
+---------------
+Created on: 28/09/2020
+---------------
+Last updated on: 28/09/2020
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
