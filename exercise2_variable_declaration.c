8593a482e004902f3881bf4384938bfdab23ee46
Kimberly <kjacques709@gmail.com>
2021-05-19 18:00:14 +0200

    Ajout de l'exercice 2: Déclaration de variables

---
+++ exercise2_variable_declaration.c
@@ -0,0 +1,27 @@
+/*
+exercise2_variable_declaration.c
+--------------
+by eldr@e@
+--------------
+Main Program
+---------------
+Created on: 28/09/2020
+---------------
+Last updated on: 28/09/2020
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{
+    int age = 21;
+    float height = 1.61;
+    char grade = 'A';
+
+    printf("Hello, I am Eldraea, I am currently %d years old, I  am  %.2f meters tall and I just got an %c in Computer Sciences! Yay!!\n", age, height, grade);
+
+    return 0;
+}
